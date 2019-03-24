e06ffe1c96be8d6a82c801059383e98b95cfe503
Fabio Matias <fabio.matias@talkdesk.com>
2019-03-24 12:53:42 +0000

    Fix resource paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+function renderApp() {
+  ReactDOM.render(<App />, rootElement);
+}
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
